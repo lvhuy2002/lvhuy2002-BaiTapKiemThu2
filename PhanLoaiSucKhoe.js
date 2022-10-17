0137fa5db55bada07759aa31f3aef6a63cbb940d
HUY <kk19elevanhuy@gmail.com>
2022-10-17 20:13:55 +0700

    bttuan 2

--- PhanLoaiSucKhoe.js
+++ PhanLoaiSucKhoe.js
@@ -1,11 +1,20 @@
 function phanLoaiSucKhoeNVQS(height, weight) {
-    if (height < 120 || weight < 20 || height > 260 || weight > 300) return -1;
-    if (height <= 152 || weight <= 39) return 6;
-    if ((height >= 153 && height <= 154) || weight === 40) return 5;
-    if ((height >= 155 && height <= 156) || (weight >= 41 && weight <= 42)) return 4;
-    if ((height >= 157 && height <= 159) || (weight >= 43 && weight <= 46)) return 3;
-    if ((height >= 160 && height <= 162) || (weight >= 47 && weight <= 50)) return 2;
-    if (height >= 163 && weight >= 51) return 1;
+    let type = -1;                                                                  //1 
+    if (height < 120 || height > 260 || weight < 20 || weight > 300)                //2
+        return type;                                                                //3
+    if ((height >= 120 && height <= 152) || (weight >= 20 && weight <= 39))         //4  
+        type = 6;                                                                   //5 
+    else if ((height >= 153 && height <= 154) || weight === 40)                     //6   
+        type = 5;                                                                   //7
+    else if ((height >= 155 && height <= 156) || (weight >= 41 && weight <= 42))    //8    
+        type = 4;                                                                   //9
+    else if ((height >= 157 && height <= 159) || (weight >= 43 && weight <= 46))    //10
+        type = 3;                                                                   //11
+    else if ((height >= 160 && height <= 162) || (weight >= 47 && weight <= 50))    //12
+        type = 2;                                                                   //13
+    else if (height >= 163 && height <= 260 && weight >= 51 && weight <= 300)       //14
+        type = 1;                                                                   //15
+    return type;                                                                    //16
 }
 
 module.exports = phanLoaiSucKhoeNVQS;
--- PhanLoaiSucKhoe.test.js
+++ PhanLoaiSucKhoe.test.js
@@ -1,54 +1,82 @@
 
 const phanLoai = require('./PhanLoaiSucKhoe');
 
-describe("Kiem thu gia tri bien", () => {
+// describe("Kiem thu gia tri bien", () => {
+//     test('T1', () => {
+//         expect(phanLoai(190, 160)).toBe(1);
+//     });
+//     test('T2', () => {
+//         expect(phanLoai(190, 20)).toBe(6);
+//     });
+//     test('T3', () => {
+//         expect(phanLoai(190, 21)).toBe(6);
+//     });
+//     test('T4', () => {
+//         expect(phanLoai(190, 300)).toBe(1);
+//     });
+//     test('T5', () => {
+//         expect(phanLoai(190, 299)).toBe(1);
+//     });
+//     test('T6', () => {
+//         expect(phanLoai(120, 160)).toBe(6);
+//     });
+//     test('T7', () => {
+//         expect(phanLoai(121, 160)).toBe(6);
+//     });
+//     test('T8', () => {
+//         expect(phanLoai(260, 160)).toBe(1);
+//     });
+//     test('T9', () => {
+//         expect(phanLoai(259, 160)).toBe(1);
+//     });
+// })
+
+// describe("Kiem thu phan hoach tuong duong", () => {
+//     test('T1', () => {
+//         expect(phanLoai(130, 150)).toBe(6);
+//     });
+//     test('T2', () => {
+//         expect(phanLoai(153, 49)).toBe(5);
+//     });
+//     test('T3', () => {
+//         expect(phanLoai(155, 44)).toBe(4);
+//     });
+//     test('T4', () => {
+//         expect(phanLoai(158, 41)).toBe(4);
+//     });
+//     test('T5', () => {
+//         expect(phanLoai(161, 40)).toBe(5);
+//     });
+//     test('T6', () => {
+//         expect(phanLoai(200, 29)).toBe(6);
+//     });
+// })
+
+describe("Kiem thu dong dieu khien (do phu C2)", () => {
     test('T1', () => {
-        expect(phanLoai(190, 160)).toBe(1);
+        expect(phanLoai(50, 50)).toBe(-1);
     });
     test('T2', () => {
-        expect(phanLoai(190, 20)).toBe(6);
+        expect(phanLoai(130, 50)).toBe(6);
     });
     test('T3', () => {
-        expect(phanLoai(190, 21)).toBe(6);
+        expect(phanLoai(153, 40)).toBe(5);
     });
     test('T4', () => {
-        expect(phanLoai(190, 300)).toBe(1);
+        expect(phanLoai(185, 42)).toBe(4);
     });
     test('T5', () => {
-        expect(phanLoai(190, 299)).toBe(1);
+        expect(phanLoai(158, 45)).toBe(3);
     });
     test('T6', () => {
-        expect(phanLoai(120, 160)).toBe(6);
+        expect(phanLoai(161, 48)).toBe(2);
     });
     test('T7', () => {
-        expect(phanLoai(121, 160)).toBe(6);
+        expect(phanLoai('a', 'b')).toBe(-1);
     });
     test('T8', () => {
-        expect(phanLoai(260, 160)).toBe(1);
-    });
-    test('T9', () => {
-        expect(phanLoai(259, 160)).toBe(1);
+        expect(phanLoai(165, 52)).toBe(1);
     });
 })
 
-describe("Kiem thu phan hoach tuong duong", () => {
-    test('T1', () => {
-        expect(phanLoai(130, 150)).toBe(6);
-    });
-    test('T2', () => {
-        expect(phanLoai(153, 49)).toBe(5);
-    });
-    test('T3', () => {
-        expect(phanLoai(155, 44)).toBe(4);
-    });
-    test('T4', () => {
-        expect(phanLoai(158, 41)).toBe(4);
-    });
-    test('T5', () => {
-        expect(phanLoai(161, 40)).toBe(5);
-    });
-    test('T6', () => {
-        expect(phanLoai(200, 29)).toBe(6);
-    });
-})
 
